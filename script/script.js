885ea6e93c84a152b2dad1174ede9655dee30fd2
KGriffey <griffey.kyle@gmail.com>
2021-12-10 13:25:35 -0700

    Refactored JS code, updated HTML divs and images to begin formatting the layout

--- script/script.js
+++ script/script.js
@@ -1,74 +1,151 @@
 /* Computer's move */
 function computerPlay() {
-    //Generate a random number between 0 and 2
+    // Generate a random number between 0 and 2
     let roll = Math.floor(Math.random() * 3);
 
-    //Depending on roll, determine the computer's move (1/3 odds per).
+    // Depending on roll, determine the computer's move (1/3 odds per).
     if (roll === 0) {
-        return "rock";
+        return 'fire';
     } else if (roll === 1) {
-        return "paper";
+        return 'water';
     } else {
-        return "scissors";
+        return 'grass';
     }
 }
 
 /* Play one round and return the result */
-function playRound(playerSelection, computerSelection) {
-    //Initialize result variable
-    let result = " ";
+function playRound() {
+    // Initialize result variable and moves
+    let result = ' ';
+    const computerSelection = computerPlay();
+    const playerSelection = this.className;
 
-    //Convert player's move to lowercase for case insensitive comparisons
-    playerSelection = playerSelection.toLowerCase();
-
-    //Check what the player's selection was, then determine the result of the game
-    if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            result = "lose";
-        } else if (computerSelection === "scissors") {
-            result = "win";
+    // Check what the player's selection against computer's, then determine the result of the game
+    if (playerSelection === 'fire') {
+        if (computerSelection === 'water') {
+            result = 'lose';
+        } else if (computerSelection === 'grass') {
+            result = 'win';
         } else {
-            result = "tie";
+            result = 'tie';
         }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            result = "win";
-        } else if (computerSelection === "scissors") {
-            result = "lose";
+    } else if (playerSelection === 'water') {
+        if (computerSelection === 'fire') {
+            result = 'win';
+        } else if (computerSelection === 'grass') {
+            result = 'lose';
         } else {
-            result = "tie";
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            result = "lose";
-        } else if (computerSelection === "paper") {
-            result = "win";
-        } else {
-            result = "tie";
+            result = 'tie';
         }
     } else {
-        //Forfeit if the move was not valid at all
-        result = "forfeit";
+        if (computerSelection === 'fire') {
+            result = 'lose';
+        } else if (computerSelection === 'water') {
+            result = 'win';
+        } else {
+            result = 'tie';
+        }
     }
 
-    //Format the player and computers moves for the result string. I.E. change "rock" to "Rock"
-    let selectionFormatter = playerSelection;
-    playerSelection = selectionFormatter.charAt(0).toUpperCase() + selectionFormatter.slice(1);
-    selectionFormatter = computerSelection;
-    computerSelection = selectionFormatter.charAt(0).toUpperCase() + selectionFormatter.slice(1);
+    // Print result
+    printResult(result);
 
-    //Return the result of the round
-    return result;
+    // Update the score
+    updateScore(result);
+
+    // At 5 wins for either player, determine who won the game
+    if (playerScore === 5 || computerScore === 5) {
+        determineWinner();
+        newGame();
+    }
+
 }
 
+/* Print round result */
+function printResult(result) {
+    const message = document.querySelector('.message');
+    message.textContent = `You ${result} this round!`;
+}
+
+/* Update the score and scoreboard */
+function updateScore(result) {
+    if (result === 'win') {
+        playerScore++;
+        const playerScoreboard = document.querySelector('.player .score');
+        playerScoreboard.textContent = playerScore;
+    } else {
+        computerScore++;
+        const computerScoreboard = document.querySelector('.computer .score');
+        computerScoreboard.textContent = computerScore;
+    }
+}
+
+/* Reset the score and scoreboard */
+function resetScore(){
+    playerScore = 0;
+    computerScore = 0;
+
+    const playerScoreboard = document.querySelector('.player .score');
+    playerScoreboard.textContent = playerScore;
+    const computerScoreboard = document.querySelector('.computer .score');
+    computerScoreboard.textContent = computerScore;
+}
+
+/* Determine the game winner and end it */
+function determineWinner() {
+    const message = document.querySelector('.message');
+    if (computerScore === 5) {
+        message.textContent = 'The computer won the game!';
+    } else {
+        message.textContent = 'You won the game!';
+    }
+
+    buttons.forEach(button => {
+        button.removeEventListener('click', playRound);
+    });
+}
+
+/* Play a new game */
+function newGame() {
+    // Add a button prompting to play again
+    const playAgain = document.querySelector('.playAgain');
+    const playAgainBtn = document.createElement('button');
+    playAgainBtn.textContent = 'Play again?';
+    playAgain.appendChild(playAgainBtn);
+
+    // Initialize a fresh game
+    playAgainBtn.addEventListener('click', initGame);
+}
+
+/* Initialize a new game */
+function initGame() {
+    
+    //Remove Play Again Button if needed
+    const playAgainBtn = document.querySelector('.playAgain button');
+    if(playAgainBtn != null){
+        const playAgain = document.querySelector('.playAgain');
+        playAgain.removeChild(playAgainBtn);
+        playAgain.textContent = ''; 
+    }
+    
+    const message = document.querySelector('.message');
+    message.textContent = 'Let\'s play a game.';
+
+    resetScore();
+
+    buttons.forEach(button => {
+        button.addEventListener('click', playRound);
+    });
+}
+
+// Initialize the first game
 let playerScore = 0;
 let computerScore = 0;
 
 const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(playRound(button.className,computerPlay()));
-    })
-});
+initGame();
+
+
+
 
 
